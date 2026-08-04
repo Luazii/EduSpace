e0681ce16a941b6dff336d2a3f2e4593523cffd1
luazii <lgumbi2169@gmail.com>
2026-08-04 12:10:00 +0200

    chore: refactor mobile monorepo configuration, update dependencies, and downgrade Tailwind CSS to version 3.4.17

--- LearniVerseNew/apps/mobile/babel.config.js
+++ LearniVerseNew/apps/mobile/babel.config.js
@@ -6,15 +6,6 @@ module.exports = function (api) {
     ],
     plugins: [
       "react-native-reanimated/plugin",
-      [
-        "module-resolver",
-        {
-          root: ["./"],
-          alias: {
-            "@": "./",
-          },
-        },
-      ],
     ],
   };
 };
